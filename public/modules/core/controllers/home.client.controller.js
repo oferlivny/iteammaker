60499a638f49a35eead73975c2469a47b810e84a
olivny <oferlivny@yahoo.com>
2015-08-13 15:37:25 +0300

    update

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -2,8 +2,32 @@
 
 
 angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-	function($scope, Authentication) {
-		// This provides Authentication context.
-		$scope.authentication = Authentication;
+	function ($scope, Authentication) {
+        // This provides Authentication context.
+        $scope.authentication = Authentication;
+
+        $scope.players = [
+            {
+                name: "1",
+                rank: 1
+            },
+            {
+                name: "2",
+                rank: 2
+            },
+            {
+                name: "3",
+                rank: 3
+            },
+            {
+                name: "4",
+                rank: 4
+            },
+            {
+                name: "5",
+                rank: 5
+            }
+            ];
+
 	}
 ]);
